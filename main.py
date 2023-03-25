300fad1a90c5722ecc3f55e0f2f51cdd457c90f2
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-25 14:50:10 +1100

    save dir

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ model = YOLO('model/best17_716.pt')
 
 
 img = st.image('images/001source.jpg')
-# res = model.predict(source='images/001source.jpg', save=True)
+res = model.predict(source='images/001source.jpg', save=True, save_dir='runs/detect')
 
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+# uploaded_file = st.file_uploader("Choose a file")
+# if uploaded_file is not None:
+#     # To read file as bytes:
+#     bytes_data = uploaded_file.getvalue()
+#     st.write(bytes_data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+#     # To convert to a string based IO:
+#     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+#     st.write(stringio)
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+#     # To read file as string:
+#     string_data = stringio.read()
+#     st.write(string_data)
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+#     # Can be used wherever a "file-like" object is accepted:
+#     dataframe = pd.read_csv(uploaded_file)
+#     st.write(dataframe)
