0e4fdd3f21ef15c64051e68b2233f7eb42037e48
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-25 22:23:03 +1100

    source image

--- app.py
+++ app.py
@@ -74,7 +74,9 @@ if source_radio == settings.IMAGE:
         else:
             if st.sidebar.button('Detect Objects'):
                 with torch.no_grad():
-                    res = model.predict(image, save=save, save_txt=save, exist_ok=True, conf=conf)
+                    # res = model.predict(image, save=save, save_txt=save, exist_ok=True, conf=conf) #original
+                    
+                    res = model.predict(source_img, save=save, save_txt=save, exist_ok=True, conf=conf)
                     boxes = res[0].boxes
                     res_plotted = res[0].plot()[:, :, ::-1]
                     st.image(res_plotted, caption='Detected Image',
@@ -92,78 +94,4 @@ if source_radio == settings.IMAGE:
                             st.write(box.xywh)
                 except Exception as ex:
                     # st.write(ex)
-                    st.write("No image is uploaded yet!")
-
-# elif source_radio == settings.VIDEO:
-#     source_vid = st.sidebar.selectbox(
-#         "Choose a video...", settings.VIDEOS_DICT.keys())
-#     video_file = open(settings.VIDEOS_DICT.get(source_vid), 'rb')
-#     video_bytes = video_file.read()
-#     st.video(video_bytes)
-#     if st.sidebar.button('Detect Video Objects'):
-#         vid_cap = cv2.VideoCapture(str(settings.VIDEOS_DICT.get(source_vid)))
-#         stframe = st.empty()
-#         while (vid_cap.isOpened()):
-#             success, image = vid_cap.read()
-#             if success:
-#                 image = cv2.resize(image, (720, int(720*(9/16))))
-#                 res = model.predict(image, conf=conf)
-#                 res_plotted = res[0].plot()
-#                 stframe.image(res_plotted,
-#                               caption='Detected Video',
-#                               channels="BGR",
-#                               use_column_width=True
-#                               )
-
-# elif source_radio == settings.WEBCAM:
-#     source_webcam = settings.WEBCAM_PATH
-#     if st.sidebar.button('Detect Objects'):
-#         vid_cap = cv2.VideoCapture(source_webcam)
-#         stframe = st.empty()
-#         while (vid_cap.isOpened()):
-#             success, image = vid_cap.read()
-#             if success:
-#                 image = cv2.resize(image, (720, int(720*(9/16))))
-#                 res = model.predict(image, conf=conf)
-#                 res_plotted = res[0].plot()
-#                 stframe.image(res_plotted,
-#                               caption='Detected Video',
-#                               channels="BGR",
-#                               use_column_width=True
-#                               )
-
-# elif source_radio == settings.RTSP:
-#     source_rtsp = st.sidebar.text_input("rtsp stream url")
-#     if st.sidebar.button('Detect Objects'):
-#         vid_cap = cv2.VideoCapture(source_rtsp)
-#         stframe = st.empty()
-#         while (vid_cap.isOpened()):
-#             success, image = vid_cap.read()
-#             if success:
-#                 image = cv2.resize(image, (720, int(720*(9/16))))
-#                 res = model.predict(image, conf=conf)
-#                 res_plotted = res[0].plot()
-#                 stframe.image(res_plotted,
-#                               caption='Detected Video',
-#                               channels="BGR",
-#                               use_column_width=True
-#                               )
-
-# elif source_radio == settings.YOUTUBE:
-#     source_youtube = st.sidebar.text_input("YouTube Video url")
-#     if st.sidebar.button('Detect Objects'):
-#         video = pafy.new(source_youtube)
-#         best = video.getbest(preftype="mp4")
-#         cap = cv2.VideoCapture(best.url)
-#         stframe = st.empty()
-#         while (cap.isOpened()):
-#             success, image = cap.read()
-#             if success:
-#                 image = cv2.resize(image, (720, int(720*(9/16))))
-#                 res = model.predict(image, conf=conf)
-#                 res_plotted = res[0].plot()
-#                 stframe.image(res_plotted,
-#                               caption='Detected Video',
-#                               channels="BGR",
-#                               use_column_width=True
-#                               )
+                    st.write("No image is uploaded yet!")                            )
