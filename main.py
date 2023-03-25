06541152c23b72274c6370e525d67004cb965da4
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-25 11:35:40 +1100

    created images folder

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from ultralytics import YOLO
+import numpy as np
+from PIL import Image
+import requests
+from io import BytesIO
+import cv2
+
+
+model = YOLO('model/best17_716.pt')
+print(model.summary())
+
 
 if st.button('Say hello'):
     st.write('Why hello there')
