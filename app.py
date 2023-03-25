7d2ba181ad15b50e66617865a516b089bdcac3e4
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-25 20:18:17 +1100

    try

--- app.py
+++ app.py
@@ -1,65 +1,160 @@
-# https://youtu.be/pI0wQbJwIIs
-"""
+from pathlib import Path
+import PIL
 
-Werkzeug provides a bunch of utilities for developing WSGI-compliant applications. 
-These utilities do things like parsing headers, sending and receiving cookies, 
-providing access to form data, generating redirects, generating error pages when 
-there's an exception, even providing an interactive debugger that runs in the browser. 
-Flask then builds upon this foundation to provide a complete web framework.
-"""
+import streamlit as st
+import torch
+import cv2
+import pafy
 
-from flask import Flask, render_template, request, redirect, flash
-from werkzeug.utils import secure_filename
-from main import getPrediction
-import os
-
-#Save images to the 'static' folder as Flask serves images from this directory
-UPLOAD_FOLDER = 'static/images/'
-
-#Create an app object using the Flask class. 
-app = Flask(__name__, static_folder="static")
-
-#Add reference fingerprint. 
-#Cookies travel with a signature that they claim to be legit. 
-#Legitimacy here means that the signature was issued by the owner of the cookie.
-#Others cannot change this cookie as it needs the secret key. 
-#It's used as the key to encrypt the session - which can be stored in a cookie.
-#Cookies should be encrypted if they contain potentially sensitive information.
-app.secret_key = "secret key"
-
-#Define the upload folder to save images uploaded by the user. 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-#Define the route to be home. 
-#The decorator below links the relative route of the URL to the function it is decorating.
-#Here, index function is with '/', our root directory. 
-#Running the app sends us to index.html.
-#Note that render_template means it looks for the file in the templates folder. 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-#Add Post method to the decorator to allow for form submission. 
-@app.route('/', methods=['POST'])
-def submit_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file:
-            filename = secure_filename(file.filename)  #Use this werkzeug method to secure filename. 
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            #getPrediction(filename)
-            label = getPrediction(filename)
-            flash(label)
-            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            flash(full_filename)
-            return redirect('/')
+import settings
+import helper
 
 
-if __name__ == "__main__":
-    app.run()
+# Sidebar
+st.title("Object Detection using YOLOv8")
+
+st.sidebar.header("ML Model Config")
+
+mlmodel_radio = st.sidebar.radio(
+    "Select Task", ['Detection', 'Segmentation'])
+conf = float(st.sidebar.slider("Select Model Confidence", 25, 100, 40)) / 100
+if mlmodel_radio == 'Detection':
+    dirpath_locator = settings.DETECT_LOCATOR
+    model_path = Path(settings.DETECTION_MODEL)
+elif mlmodel_radio == 'Segmentation':
+    dirpath_locator = settings.SEGMENT_LOCATOR
+    model_path = Path(settings.SEGMENTATION_MODEL)
+try:
+    model = helper.load_model(model_path)
+except Exception as ex:
+    print(ex)
+    st.write(f"Unable to load model. Check the specified path: {model_path}")
+
+source_img = None
+st.sidebar.header("Image/Video Config")
+source_radio = st.sidebar.radio(
+    "Select Source", settings.SOURCES_LIST)
+
+# body
+# If image is selected
+if source_radio == settings.IMAGE:
+    source_img = st.sidebar.file_uploader(
+        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+    save_radio = st.sidebar.radio("Save image to download", ["Yes", "No"])
+    save = True if save_radio == 'Yes' else False
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if source_img is None:
+            default_image_path = str(settings.DEFAULT_IMAGE)
+            image = PIL.Image.open(default_image_path)
+            st.image(default_image_path, caption='Default Image',
+                     use_column_width=True)
+        else:
+            image = PIL.Image.open(source_img)
+            st.image(source_img, caption='Uploaded Image',
+                     use_column_width=True)
+
+    with col2:
+        if source_img is None:
+            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
+            image = PIL.Image.open(default_detected_image_path)
+            st.image(default_detected_image_path, caption='Detected Image',
+                     use_column_width=True)
+        else:
+            if st.sidebar.button('Detect Objects'):
+                with torch.no_grad():
+                    res = model.predict(
+                        image, save=save, save_txt=save, exist_ok=True, conf=conf)
+                    boxes = res[0].boxes
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption='Detected Image',
+                             use_column_width=True)
+                    IMAGE_DOWNLOAD_PATH = f"runs/{dirpath_locator}/predict/image0.jpg"
+                    with open(IMAGE_DOWNLOAD_PATH, 'rb') as fl:
+                        st.download_button("Download object-detected image",
+                                           data=fl,
+                                           file_name="image0.jpg",
+                                           mime='image/jpg'
+                                           )
+                try:
+                    with st.expander("Detection Results"):
+                        for box in boxes:
+                            st.write(box.xywh)
+                except Exception as ex:
+                    # st.write(ex)
+                    st.write("No image is uploaded yet!")
+
+elif source_radio == settings.VIDEO:
+    source_vid = st.sidebar.selectbox(
+        "Choose a video...", settings.VIDEOS_DICT.keys())
+    video_file = open(settings.VIDEOS_DICT.get(source_vid), 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
+    if st.sidebar.button('Detect Video Objects'):
+        vid_cap = cv2.VideoCapture(str(settings.VIDEOS_DICT.get(source_vid)))
+        stframe = st.empty()
+        while (vid_cap.isOpened()):
+            success, image = vid_cap.read()
+            if success:
+                image = cv2.resize(image, (720, int(720*(9/16))))
+                res = model.predict(image, conf=conf)
+                res_plotted = res[0].plot()
+                stframe.image(res_plotted,
+                              caption='Detected Video',
+                              channels="BGR",
+                              use_column_width=True
+                              )
+
+elif source_radio == settings.WEBCAM:
+    source_webcam = settings.WEBCAM_PATH
+    if st.sidebar.button('Detect Objects'):
+        vid_cap = cv2.VideoCapture(source_webcam)
+        stframe = st.empty()
+        while (vid_cap.isOpened()):
+            success, image = vid_cap.read()
+            if success:
+                image = cv2.resize(image, (720, int(720*(9/16))))
+                res = model.predict(image, conf=conf)
+                res_plotted = res[0].plot()
+                stframe.image(res_plotted,
+                              caption='Detected Video',
+                              channels="BGR",
+                              use_column_width=True
+                              )
+
+elif source_radio == settings.RTSP:
+    source_rtsp = st.sidebar.text_input("rtsp stream url")
+    if st.sidebar.button('Detect Objects'):
+        vid_cap = cv2.VideoCapture(source_rtsp)
+        stframe = st.empty()
+        while (vid_cap.isOpened()):
+            success, image = vid_cap.read()
+            if success:
+                image = cv2.resize(image, (720, int(720*(9/16))))
+                res = model.predict(image, conf=conf)
+                res_plotted = res[0].plot()
+                stframe.image(res_plotted,
+                              caption='Detected Video',
+                              channels="BGR",
+                              use_column_width=True
+                              )
+
+elif source_radio == settings.YOUTUBE:
+    source_youtube = st.sidebar.text_input("YouTube Video url")
+    if st.sidebar.button('Detect Objects'):
+        video = pafy.new(source_youtube)
+        best = video.getbest(preftype="mp4")
+        cap = cv2.VideoCapture(best.url)
+        stframe = st.empty()
+        while (cap.isOpened()):
+            success, image = cap.read()
+            if success:
+                image = cv2.resize(image, (720, int(720*(9/16))))
+                res = model.predict(image, conf=conf)
+                res_plotted = res[0].plot()
+                stframe.image(res_plotted,
+                              caption='Detected Video',
+                              channels="BGR",
+                              use_column_width=True
+                              )
--- main.py
+++ main.py
@@ -1,102 +1,160 @@
+from pathlib import Path
+import PIL
+
 import streamlit as st
-from ultralytics import YOLO
-import numpy as np
-from PIL import Image
-import requests
-from io import BytesIO
+import torch
 import cv2
-import pandas as pd
-from io import StringIO
-import base64
-import io
-import glob
-from base64 import decodebytes
-from io import BytesIO
-import matplotlib.pyplot as plt
+# import pafy
+
+import settings
+import helper
 
 
+# Sidebar
+st.title("Object Detection using YOLOv8")
 
+st.sidebar.header("ML Model Config")
 
+mlmodel_radio = st.sidebar.radio(
+    "Select Task", ['Detection', 'Segmentation'])
+conf = float(st.sidebar.slider("Select Model Confidence", 25, 100, 40)) / 100
+if mlmodel_radio == 'Detection':
+    dirpath_locator = settings.DETECT_LOCATOR
+    model_path = Path(settings.DETECTION_MODEL)
+elif mlmodel_radio == 'Segmentation':
+    dirpath_locator = settings.SEGMENT_LOCATOR
+    model_path = Path(settings.SEGMENTATION_MODEL)
+try:
+    model = helper.load_model(model_path)
+except Exception as ex:
+    print(ex)
+    st.write(f"Unable to load model. Check the specified path: {model_path}")
 
-# model = YOLO('model/best17_716.pt')
-# print(model.summary())
+source_img = None
+st.sidebar.header("Image/Video Config")
+source_radio = st.sidebar.radio(
+    "Select Source", settings.SOURCES_LIST)
 
+# body
+# If image is selected
+if source_radio == settings.IMAGE:
+    source_img = st.sidebar.file_uploader(
+        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+    save_radio = st.sidebar.radio("Save image to download", ["Yes", "No"])
+    save = True if save_radio == 'Yes' else False
+    col1, col2 = st.columns(2)
 
-# img = st.image('images/001source.jpg')
-# res = model.predict(source='images/001source.jpg', save=True)
-# boxes = res[0].boxes
-# box = boxes[0]  # returns one box
-# # box.xyxy
-# # boxes.xyxy  # box with xyxy format, (N, 4)
-# # boxes.xywh  # box with xywh format, (N, 4)
-# # boxes.xyxyn  # box with xyxy format but normalized, (N, 4)
-# # boxes.xywhn  # box with xywh format but normalized, (N, 4)
-# # boxes.conf  # confidence score, (N, 1)
-# # boxes.cls  # cls, (N, 1)
-# boxes.data  # raw bboxes tensor, (N, 6) or boxes.boxes .
-# st.dataframe(boxes.data)
+    with col1:
+        if source_img is None:
+            default_image_path = str(settings.DEFAULT_IMAGE)
+            image = PIL.Image.open(default_image_path)
+            st.image(default_image_path, caption='Default Image',
+                     use_column_width=True)
+        else:
+            image = PIL.Image.open(source_img)
+            st.image(source_img, caption='Uploaded Image',
+                     use_column_width=True)
 
+    with col2:
+        if source_img is None:
+            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
+            image = PIL.Image.open(default_detected_image_path)
+            st.image(default_detected_image_path, caption='Detected Image',
+                     use_column_width=True)
+        else:
+            if st.sidebar.button('Detect Objects'):
+                with torch.no_grad():
+                    res = model.predict(
+                        image, save=save, save_txt=save, exist_ok=True, conf=conf)
+                    boxes = res[0].boxes
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption='Detected Image',
+                             use_column_width=True)
+                    IMAGE_DOWNLOAD_PATH = f"runs/{dirpath_locator}/predict/image0.jpg"
+                    with open(IMAGE_DOWNLOAD_PATH, 'rb') as fl:
+                        st.download_button("Download object-detected image",
+                                           data=fl,
+                                           file_name="image0.jpg",
+                                           mime='image/jpg'
+                                           )
+                try:
+                    with st.expander("Detection Results"):
+                        for box in boxes:
+                            st.write(box.xywh)
+                except Exception as ex:
+                    # st.write(ex)
+                    st.write("No image is uploaded yet!")
 
+# elif source_radio == settings.VIDEO:
+#     source_vid = st.sidebar.selectbox(
+#         "Choose a video...", settings.VIDEOS_DICT.keys())
+#     video_file = open(settings.VIDEOS_DICT.get(source_vid), 'rb')
+#     video_bytes = video_file.read()
+#     st.video(video_bytes)
+#     if st.sidebar.button('Detect Video Objects'):
+#         vid_cap = cv2.VideoCapture(str(settings.VIDEOS_DICT.get(source_vid)))
+#         stframe = st.empty()
+#         while (vid_cap.isOpened()):
+#             success, image = vid_cap.read()
+#             if success:
+#                 image = cv2.resize(image, (720, int(720*(9/16))))
+#                 res = model.predict(image, conf=conf)
+#                 res_plotted = res[0].plot()
+#                 stframe.image(res_plotted,
+#                               caption='Detected Video',
+#                               channels="BGR",
+#                               use_column_width=True
+#                               )
 
+# elif source_radio == settings.WEBCAM:
+#     source_webcam = settings.WEBCAM_PATH
+#     if st.sidebar.button('Detect Objects'):
+#         vid_cap = cv2.VideoCapture(source_webcam)
+#         stframe = st.empty()
+#         while (vid_cap.isOpened()):
+#             success, image = vid_cap.read()
+#             if success:
+#                 image = cv2.resize(image, (720, int(720*(9/16))))
+#                 res = model.predict(image, conf=conf)
+#                 res_plotted = res[0].plot()
+#                 stframe.image(res_plotted,
+#                               caption='Detected Video',
+#                               channels="BGR",
+#                               use_column_width=True
+#                               )
 
+# elif source_radio == settings.RTSP:
+#     source_rtsp = st.sidebar.text_input("rtsp stream url")
+#     if st.sidebar.button('Detect Objects'):
+#         vid_cap = cv2.VideoCapture(source_rtsp)
+#         stframe = st.empty()
+#         while (vid_cap.isOpened()):
+#             success, image = vid_cap.read()
+#             if success:
+#                 image = cv2.resize(image, (720, int(720*(9/16))))
+#                 res = model.predict(image, conf=conf)
+#                 res_plotted = res[0].plot()
+#                 stframe.image(res_plotted,
+#                               caption='Detected Video',
+#                               channels="BGR",
+#                               use_column_width=True
+#                               )
 
-# https://youtu.be/pI0wQbJwIIs
-"""
-For training, watch videos (202 and 203): 
-    https://youtu.be/qB6h5CohLbs
-    https://youtu.be/fyZ9Rxpoz2I
-
-The 7 classes of skin cancer lesions included in this dataset are:
-Melanocytic nevi (nv)
-Melanoma (mel)
-Benign keratosis-like lesions (bkl)
-Basal cell carcinoma (bcc) 
-Actinic keratoses (akiec)
-Vascular lesions (vas)
-Dermatofibroma (df)
-
-"""
-
-
-
-# import numpy as np
-# from PIL import Image
-# from sklearn.preprocessing import LabelEncoder
-# from tensorflow.keras.models import load_model
-
-
-def getPrediction(filename):
-    
-    # classes = ['Actinic keratoses', 'Basal cell carcinoma', 
-    #            'Benign keratosis-like lesions', 'Dermatofibroma', 'Melanoma', 
-    #            'Melanocytic nevi', 'Vascular lesions']
-    # le = LabelEncoder()
-    # le.fit(classes)
-    # le.inverse_transform([2])
-    
-    
-    #Load model
-    # my_model=load_model("model/HAM10000_100epochs.h5")
-    my_model=YOLO('model/best17_716.pt')
-
-    
-    # SIZE = 32 #Resize to same size as training images
-    img_path = 'static/images/'+filename
-    # img = np.asarray(Image.open(img_path).resize((SIZE,SIZE)))
-    
-    # img = img/255.      #Scale pixel values
-    # 
-    # img = np.expand_dims(img, axis=0)  #Get it tready as input to the network       
-    
-    img=img_path
-    pred = my_model.predict(img) #Predict                    
-    return pred
-
-    #Convert prediction to class name
-    # pred_class = le.inverse_transform([np.argmax(pred)])[0]
-    # print("Diagnosis is:", pred_class)
-    # return pred_class
-
-
-#test_prediction =getPrediction('example.jpg')
-
+# elif source_radio == settings.YOUTUBE:
+#     source_youtube = st.sidebar.text_input("YouTube Video url")
+#     if st.sidebar.button('Detect Objects'):
+#         video = pafy.new(source_youtube)
+#         best = video.getbest(preftype="mp4")
+#         cap = cv2.VideoCapture(best.url)
+#         stframe = st.empty()
+#         while (cap.isOpened()):
+#             success, image = cap.read()
+#             if success:
+#                 image = cv2.resize(image, (720, int(720*(9/16))))
+#                 res = model.predict(image, conf=conf)
+#                 res_plotted = res[0].plot()
+#                 stframe.image(res_plotted,
+#                               caption='Detected Video',
+#                               channels="BGR",
+#                               use_column_width=True
+#                               )
