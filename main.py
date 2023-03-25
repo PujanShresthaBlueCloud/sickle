a49f70c30e00532f130bd8ab72125eb6dde1214f
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-25 19:11:28 +1100

    check

--- main.py
+++ main.py
@@ -38,168 +38,65 @@ img = st.image('images/001source.jpg')
 
 
 
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-#     # To read file as bytes:
-#     bytes_data = uploaded_file.getvalue()
-#     st.write(bytes_data)
-
-#     # To convert to a string based IO:
-#     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-#     st.write(stringio)
-
-#     # To read file as string:
-#     string_data = stringio.read()
-#     st.write(string_data)
-
-#     # Can be used wherever a "file-like" object is accepted:
-#     dataframe = pd.read_csv(uploaded_file)
-#     st.write(dataframe)
 
 
-
+# https://youtu.be/pI0wQbJwIIs
 """
-    FROM https://github.com/matthewbrems/streamlit-bccd/blob/master/streamlit_app.py
+For training, watch videos (202 and 203): 
+    https://youtu.be/qB6h5CohLbs
+    https://youtu.be/fyZ9Rxpoz2I
+
+The 7 classes of skin cancer lesions included in this dataset are:
+Melanocytic nevi (nv)
+Melanoma (mel)
+Benign keratosis-like lesions (bkl)
+Basal cell carcinoma (bcc) 
+Actinic keratoses (akiec)
+Vascular lesions (vas)
+Dermatofibroma (df)
+
 """
 
 
-##########
-##### Set up sidebar.
-##########
 
-# Add in location to select image.
-
-st.sidebar.write('#### Select an image to upload.')
-uploaded_file = st.sidebar.file_uploader('',
-                                         type=['png', 'jpg', 'jpeg'],
-                                         accept_multiple_files=False)
-
-# st.sidebar.write('[Find additional images on Roboflow.](https://public.roboflow.com/object-detection/bccd/)')
-
-## Add in sliders.
-confidence_threshold = st.sidebar.slider('Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0, 1.0, 0.5, 0.01)
-overlap_threshold = st.sidebar.slider('Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5, 0.01)
+import numpy as np
+from PIL import Image
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import load_model
 
 
-# image = Image.open('./images/roboflow_logo.png')
-# st.sidebar.image(image,
-#                  use_column_width=True)
+def getPrediction(filename):
+    
+    classes = ['Actinic keratoses', 'Basal cell carcinoma', 
+               'Benign keratosis-like lesions', 'Dermatofibroma', 'Melanoma', 
+               'Melanocytic nevi', 'Vascular lesions']
+    le = LabelEncoder()
+    le.fit(classes)
+    le.inverse_transform([2])
+    
+    
+    #Load model
+    # my_model=load_model("model/HAM10000_100epochs.h5")
+    my_model=YOLO('model/best17_716.pt')
 
-# image = Image.open('./images/streamlit_logo.png')
-# st.sidebar.image(image,
-#                  use_column_width=True)
+    
+    # SIZE = 32 #Resize to same size as training images
+    img_path = 'static/images/'+filename
+    # img = np.asarray(Image.open(img_path).resize((SIZE,SIZE)))
+    
+    # img = img/255.      #Scale pixel values
+    # 
+    # img = np.expand_dims(img, axis=0)  #Get it tready as input to the network       
+    
+    img=img_path
+    pred = my_model.predict(img) #Predict                    
+    return pred
 
-
-
-
-
-
-
-##########
-##### Set up main app.
-##########
-
-## Title.
-st.write('# Sickle Cell Detection')
-
-## Pull in default image or user-selected image.
-if uploaded_file is None:
-    # Default image.
-    # url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
-    url = 'images/001source.jpg'
-    image = Image.open(requests.get(url, stream=True).raw)
-
-else:
-    # User-selected image.
-    image = Image.open(uploaded_file)
-
-## Subtitle.
-st.write('### Inferenced Image')
-
-# Convert to JPEG Buffer.
-# buffered = io.BytesIO()
-# image.save(buffered, quality=90, format='JPEG')
-
-# Base 64 encode.
-# img_str = base64.b64encode(buffered.getvalue())
-# img_str = img_str.decode('ascii')
-
-## Construct the URL to retrieve image.
-# upload_url = ''.join([
-#     'https://infer.roboflow.com/rf-bccd-bkpj9--1',
-#     f'?access_token={st.secrets["access_token"]}',
-#     '&format=image',
-#     f'&overlap={overlap_threshold * 100}',
-#     f'&confidence={confidence_threshold * 100}',
-#     '&stroke=2',
-#     '&labels=True'
-# ])
-
-# upload_url='images/'
-
-## POST to the API.
-# r = requests.post(upload_url,
-#                   data=img_str,
-#                   headers={
-#     'Content-Type': 'application/x-www-form-urlencoded'
-# })
-
-res = model.predict(source=uploaded_file, save=True)
-boxes = res[0].boxes
-box = boxes[0]  # returns one box
-# box.xyxy
-# boxes.xyxy  # box with xyxy format, (N, 4)
-# boxes.xywh  # box with xywh format, (N, 4)
-# boxes.xyxyn  # box with xyxy format but normalized, (N, 4)
-# boxes.xywhn  # box with xywh format but normalized, (N, 4)
-# boxes.conf  # confidence score, (N, 1)
-# boxes.cls  # cls, (N, 1)
-boxes.data  # raw bboxes tensor, (N, 6) or boxes.boxes .
-st.dataframe(boxes.data)
+    #Convert prediction to class name
+    # pred_class = le.inverse_transform([np.argmax(pred)])[0]
+    # print("Diagnosis is:", pred_class)
+    # return pred_class
 
 
-# image = Image.open(BytesIO(r.content))
-# image = Image.open(BytesIO(res.content))
+#test_prediction =getPrediction('example.jpg')
 
-# Convert to JPEG Buffer.
-# buffered = io.BytesIO()
-# image.save(buffered, quality=90, format='JPEG')
-
-# Display image.
-st.image(image,
-         use_column_width=True)
-
-## Construct the URL to retrieve JSON.
-# upload_url = ''.join([
-#     'https://infer.roboflow.com/rf-bccd-bkpj9--1',
-#     f'?access_token={st.secrets["access_token"]}'
-# ])
-
-## POST to the API.
-# r = requests.post(upload_url,
-#                   data=img_str,
-#                   headers={
-#     'Content-Type': 'application/x-www-form-urlencoded'
-# })
-
-
-## Save the JSON.
-# output_dict = r.json()
-
-## Generate list of confidences.
-# confidences = [box['confidence'] for box in output_dict['predictions']]
-
-## Summary statistics section in main app.
-st.write('### Summary Statistics')
-# st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
-# st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
-
-## Histogram in main app.
-st.write('### Histogram of Confidence Levels')
-# fig, ax = plt.subplots()
-# ax.hist(confidences, bins=10, range=(0.0,1.0))
-# st.pyplot(fig)
-
-## Display the JSON in main app.
-st.write('### JSON Output')
-# st.write(r.json())
