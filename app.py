c34806ff233fbe191f83673273ac00a26a6d2cde
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-26 17:39:06 +1100

    try

--- app.py
+++ app.py
@@ -128,11 +128,11 @@ if source_radio == settings.IMAGE:
     with st.container():
         if detect_objects:
             total_detection = len(Normal)+len(Sickle)+len(Target)+len(Crystal)+len(others)
-            st.write("Normal: ", len(Normal), (len(Normal)/total_detection)*100%.2f," %")
-            st.write("Sickle: ", len(Sickle), (len(Sickle)/total_detection)*100%.2f," %")
-            st.write("Target: ", len(Target), (len(Target)/total_detection)*100%.2f," %")
-            st.write("Crystal: ", len(Crystal), (len(Crystal)/total_detection)*100%.2f," %")
-            st.write("others: ", len(others), (len(others)/total_detection)*100%.2f," %")
+            st.write("Normal: ", len(Normal), "%.2f" % ((len(Normal)/total_detection)*100)," %")
+            st.write("Sickle: ", len(Sickle), "%.2f" % ((len(Sickle)/total_detection)*100)," %")
+            st.write("Target: ", len(Target), "%.2f" % ((len(Target)/total_detection)*100)," %")
+            st.write("Crystal: ", len(Crystal), "%.2f" % ((len(Crystal)/total_detection)*100)," %")
+            st.write("others: ", len(others), "%.2f" % ((len(others)/total_detection)*100)," %")
             st.write("Total detected", total_detection)
 
         else:
