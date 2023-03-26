5d777b478ce8483167f441c371c0e9ac72afeb4f
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-26 15:21:02 +1100

    ddkk

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ if source_radio == settings.IMAGE:
         # box_df = pd.DataFrame(boxes.cls, columns=boxes.index)
         classes = ['Normal','Sickle','Target','Crystal','others']
         for cls in boxes.cls:
-            print(cls)
+            st.write(cls)
