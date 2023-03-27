d088a76637b1cbe5d0b953d212afcbe4d4ea4324
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-28 10:36:36 +1100

    replace df

--- pages/eda.py
+++ pages/eda.py
@@ -13,6 +13,7 @@ with header:
     st.title("Exploratory Data Analysis")
 
 with dataset:
+    st.header("Slice2 dataset label")
     csv = settings.CSV
     label_df = pd.read_csv(csv)
     label_df.loc[label_df['label_name'] == 'Normal', 'label_name'] = 0
@@ -20,5 +21,6 @@ with dataset:
     label_df.loc[label_df['label_name'] == 'Target', 'label_name'] = 2
     label_df.loc[label_df['label_name'] == 'Crystal', 'label_name'] = 3
     label_df.loc[label_df['label_name'] == 'Others', 'label_name'] = 4
-    print(label_df)
     st.dataframe(label_df)
+    label_name_dist=label_df['label_name'].value_counts()
+    st.bar_chart(label_name_dist)
