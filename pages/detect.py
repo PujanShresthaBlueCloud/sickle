2c564c42d9dba97568fde98b2634f5abb54b0b05
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-27 16:11:00 +1100

    added table

--- pages/detect.py
+++ pages/detect.py
@@ -151,22 +151,17 @@ if source_radio == settings.IMAGE:
             st.write("others: ", others_count, others_percent, " %")
             st.write("Total detected ", total_detection, ", at confidence ", "%.2f" %(conf * 100)," %")
 
-            # Colons can be used to align columns.
+            st.write(""" 
+                Colons can be used to align columns.
 
-            | Tables        | Are           | Cool  |
-            | ------------- |:-------------:| -----:|
-            | col 3 is      | right-aligned | $1600 |
-            | col 2 is      | centered      |   $12 |
-            | zebra stripes | are neat      |    $1 |
-
-            # There must be at least 3 dashes separating each header cell.
-            # The outer pipes (|) are optional, and you don't need to make the 
-            # raw Markdown line up prettily. You can also use inline Markdown.
-
-            Markdown | Less | Pretty
-            --- | --- | ---
-            *Still* | `renders` | **nicely**
-            1 | 2 | 3
+                | *Class*        | *Count*          | *Percent*  |
+                | ------------- |:-------------:| -----:|
+                | Normal    | {normal_count} | {normal_percent} % |
+                | Sickle      | {sickle_count}      |   {sickle_percent} % |
+                | Target | {target_count}     |    {target_percent} % |
+                | Crystal | {crystal_count}      |    {crystal_percent} % |
+                | Others | {others_count}      |    {others_percent} % |
+            """)
 
         else:
             st.write('')
