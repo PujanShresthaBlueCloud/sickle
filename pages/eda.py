bad0224550bc70f47fc5e1aba289dfdc272fbde8
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-28 08:44:43 +1100

    categorize

--- pages/eda.py
+++ pages/eda.py
@@ -4,8 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import settings
 
-st.write("Exploratory Data Analysis")
-csv = settings.CSV
-label_data_frame = pd.read_csv(csv)
-label_data_frame.head()
-st.dataframe(label_data_frame)
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+
+
+with header:
+    st.title("Exploratory Data Analysis")
+
+with dataset:
+    csv = settings.CSV
+    label_data_frame = pd.read_csv(csv)
+    label_data_frame.head()
+    st.dataframe(label_data_frame)
+    class_df = label_data_frame['class'].astype("category")
+    st.write(class_df)
