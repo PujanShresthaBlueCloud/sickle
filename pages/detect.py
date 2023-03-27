1a1b69bf0beec2bbf80e0219a2fa7c38da9e906d
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-27 16:02:26 +1100

    added table

--- pages/detect.py
+++ pages/detect.py
@@ -151,5 +151,22 @@ if source_radio == settings.IMAGE:
             st.write("others: ", others_count, others_percent, " %")
             st.write("Total detected ", total_detection, ", at confidence ", "%.2f" %(conf * 100)," %")
 
+            # Colons can be used to align columns.
+
+            | Tables        | Are           | Cool  |
+            | ------------- |:-------------:| -----:|
+            | col 3 is      | right-aligned | $1600 |
+            | col 2 is      | centered      |   $12 |
+            | zebra stripes | are neat      |    $1 |
+
+            # There must be at least 3 dashes separating each header cell.
+            # The outer pipes (|) are optional, and you don't need to make the 
+            # raw Markdown line up prettily. You can also use inline Markdown.
+
+            Markdown | Less | Pretty
+            --- | --- | ---
+            *Still* | `renders` | **nicely**
+            1 | 2 | 3
+
         else:
             st.write('')
