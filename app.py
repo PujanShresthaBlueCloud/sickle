1b1867f3f15a0f5b339d429da480423c587e120a
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-29 09:53:24 +1100

    added tableau requirement

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ from ultralytics import YOLO
 import pandas as pd
 import numpy as np
 
+# Load Tableau JavaScript and CSS
+import streamlit.components.v1 as components
+components.html(
+    """
+    <script type='text/javascript' src='https://tableau.com/javascripts/api/tableau-2.min.js'></script>
+    <link rel='stylesheet' type='text/css' href='https://tableau.com/javascripts/api/tableau-2.min.css' />
+    """
+)
+
 container = st.container()
 
 with container:
@@ -23,3 +32,27 @@ with container:
     st.header("The solution")
     st.text("Applying Computer Vision For Red Blood Cell Classification")
     st.text("To Diagnose Sickle Cell Disease")
+
+
+# Initialize Tableau visualization
+def init_viz():
+    viz_url = "https://public.tableau.com/views/MyWorkbook/MyVisualization"
+    viz_options = {
+        'hideTabs': True,
+        'width': '800px',
+        'height': '600px',
+        'onFirstInteractive': on_first_interactive
+    }
+    viz_container = components.declare_component("viz", url=viz_url, options=viz_options)
+    return viz_container
+
+# Define callback function
+def on_first_interactive():
+    workbook = viz.getWorkbook()
+    active_sheet = workbook.getActiveSheet()
+    active_sheet.applyFilterAsync("Region", "West", tableau.FilterUpdateType.REPLACE)
+
+
+# Call the init_viz function to display the visualization
+viz = init_viz()
+viz
