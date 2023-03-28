06a0debc8394510bd428830a6a9ede743df146e6
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-28 14:37:50 +1100

    added text

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ from ultralytics import YOLO
 import pandas as pd
 import numpy as np
 
+container = st.container()
 
-# Sidebar
-st.title("Diagnose Sickle Cell Disease Using YOLOV8")
-st.header("The problem")
-st.text("Sickle Cell Disease (SCD) a genetic disease;")
-st.text("✓ Affects red blood cells (RBC), resulting in RBC disorder illness;")
-st.text("✓ 1000 children born disease in Africa daily;")
-st.text("✓ Difficult for developing countries' populations to access medical diagnoses;")
-st.text("✓ Remote medical centers and costly electrophoresis tests in rural areas.")
+with container:
+    st.title("Diagnose Sickle Cell Disease Using YOLOV8")
+    st.header("The problem")
+    st.text("✓ Sickle Cell Disease (SCD) a genetic disease;")
+    st.text("✓ Affects red blood cells (RBC), resulting in RBC disorder illness;")
+    st.text("✓ 1000 children born disease in Africa daily;")
+    st.text("✓ Difficult for developing countries' populations to access medical diagnoses;")
+    st.text("✓ Remote medical centers and costly electrophoresis tests in rural areas.")
 
-st.header("The solution")
-st.text("Applying Computer Vision For Red Blood Cell Classification To Diagnose Sickle Cell Disease")
+    st.header("The solution")
+    st.text("Applying Computer Vision For Red Blood Cell Classification To Diagnose Sickle Cell Disease")
