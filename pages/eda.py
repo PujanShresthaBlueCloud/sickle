3286f3fb6043f60727f4f769586c64f16d5af75e
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-28 16:55:56 +1100

    describe

--- pages/eda.py
+++ pages/eda.py
@@ -36,7 +36,7 @@ with dataset:
     st.dataframe(label_df)
     st.text("Information ")
     # st.write(label_df.info())
-    st.write(label_df.describe)
+    st.write(label_df.describe())
 
     label_name_dist=label_df['label_name'].value_counts()
     
