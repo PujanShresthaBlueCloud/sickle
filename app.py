a0d8ebcccff6f849d84d2d9ff67238a31363a3ba
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-28 14:36:23 +1100

    added text

--- app.py
+++ app.py
@@ -13,8 +13,11 @@ import numpy as np
 # Sidebar
 st.title("Diagnose Sickle Cell Disease Using YOLOV8")
 st.header("The problem")
-st.text("Sickle Cell Disease (SCD) a genetic disease; ✓ Affects red blood cells (RBC), resulting in RBC disorder illness;✓ 1000 children born disease in Africa daily;")
-st.text("✓ Difficult for developing countries' populations to access medical diagnoses; ✓ Remote medical centers and costly electrophoresis tests in rural areas.")
+st.text("Sickle Cell Disease (SCD) a genetic disease;")
+st.text("✓ Affects red blood cells (RBC), resulting in RBC disorder illness;")
+st.text("✓ 1000 children born disease in Africa daily;")
+st.text("✓ Difficult for developing countries' populations to access medical diagnoses;")
+st.text("✓ Remote medical centers and costly electrophoresis tests in rural areas.")
 
 st.header("The solution")
 st.text("Applying Computer Vision For Red Blood Cell Classification To Diagnose Sickle Cell Disease")
