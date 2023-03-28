b6f926e4aa038b7be650fe131bcc1ee53a261d7d
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-28 11:28:51 +1100

    info

--- pages/eda.py
+++ pages/eda.py
@@ -16,15 +16,17 @@ with dataset:
     st.header("Slice2 dataset label")
     csv = settings.CSV
     label_df = pd.read_csv(csv)
-    label_df.loc[label_df['label_name'] == 'Normal', 'label_name'] = 1
-    label_df.loc[label_df['label_name'] == 'Sickle', 'label_name'] = 2
-    label_df.loc[label_df['label_name'] == 'Target', 'label_name'] = 3
-    label_df.loc[label_df['label_name'] == 'Crystal', 'label_name'] = 4
-    label_df.loc[label_df['label_name'] == 'Others', 'label_name'] = 5
+    # label_df.loc[label_df['label_name'] == 'Normal', 'label_name'] = 1
+    # label_df.loc[label_df['label_name'] == 'Sickle', 'label_name'] = 2
+    # label_df.loc[label_df['label_name'] == 'Target', 'label_name'] = 3
+    # label_df.loc[label_df['label_name'] == 'Crystal', 'label_name'] = 4
+    # label_df.loc[label_df['label_name'] == 'Others', 'label_name'] = 5
     st.dataframe(label_df)
+    st.text("Information ")
+    st.write(label_df.info())
+    st.write(label_df.describe)
+
     label_name_dist=label_df['label_name'].value_counts()
     st.text('Log transformation of label name')
     st.bar_chart(np.log(label_name_dist))
-    label_df_drop=label_df.drop(['image_name', 'image_width', 'image_height','label_name'], axis=1)
-    st.text('Line chart')
-    st.bar_chart(label_df_drop, use_container_width=True)
+
