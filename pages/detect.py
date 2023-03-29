961a453f29d056b8c1eebaa9ed6761695ca5c6fe
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-03-29 13:47:49 +1100

    graph count

--- pages/detect.py
+++ pages/detect.py
@@ -138,10 +138,10 @@ if source_radio == settings.IMAGE:
             st.header("Class percent")
             labels = detected_data_frame['class']
             sizes = detected_data_frame['percent']
-            explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+            # explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
             fig1, ax1 = plt.subplots()
-            ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            ax1.pie(sizes, explode=1, labels=labels, autopct='%1.1f%%',
                     shadow=True, startangle=90)
             ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
