47cba3aa08fa8abdb6e36345ef2ed92387ef1dc6
P <pujan_sth@yahoo.com>
2023-03-29 20:53:54 +1100

    added tableau check connection

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ with container:
 # Initialize Tableau visualization
 def init_viz():
     viz_url = "https://public.tableau.com/views/MyWorkbook/MyVisualization"
+    viz_url = "https://prod-apsoutheast-a.online.tableau.com/t/kaalakoota/newWorkbook/1gsmbqrg4$l1w9-i5-h2-fe-397c38#1"
     viz_options = {
         'hideTabs': True,
         'width': '800px',
