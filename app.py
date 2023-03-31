240c80a55fe9af33ac86863825af2dbf77bcc46c
P <pujan_sth@yahoo.com>
2023-03-31 21:14:44 +1100

    added style

--- app.py
+++ app.py
@@ -8,18 +8,36 @@ import helper
 from ultralytics import YOLO
 import pandas as pd
 import numpy as np
+import streamlit.components.v1 as components
 
 st.markdown("""
     <style>
     .main {
-    background-color:#F5F5F5;
-    margin:0px;
-    padding:0px;
+        background-color:#F5F5F5;
+        margin:0px;
+        padding:0px;
+    }
+    .css-1y4p8pa {
+        width: 100%;
+        padding: 6rem 1rem 10rem 1rem;
+        max-width: 90rem;
+    }
+    .styles_terminalButton__3xUnY {
+        display: none;
     }
     </style>
+
     """, unsafe_allow_html=True
 )
-
+# components.html("""
+#     <script>
+#         const elements = window.parent.document.querySelectorAll()
+#         elements[0]
+#     </script>
+# """,
+#     height=0,
+#     width=0
+# )
 container = st.container()
 with container:
     st.title("Diagnose Sickle Cell Disease")
