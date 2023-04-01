be76fa96728632649b6b34710560f8376a3a4e07
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-01 11:16:45 +1100

    edit style

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@
 function todayDate(){
     var d = new Date();
     var n = d.getFullYear() + "  ";
-    return  n;
+    return  window.parent.document.getElementsByTagName("em").innerHTML=n;
   }
 const elements = window.parent.document.getElementsByTagName('footer')
-elements[0].innerHTML = "&copy; Omdena project" 
-elements[1].innerHTML = todayDate()
+elements[0].innerHTML = "&copy; Omdena project <em></em>" 
