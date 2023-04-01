0eb490b1b3d74b7b98a1847ddfa88665ad0aafbc
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-01 11:35:52 +1100

    edit style

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 
-function insertParagraph(){
-    var newElement = window.parent.document.createElement("p");
-    // var node = document.createTextNode("new text");
-    // newElement.appendChild(node);
-    var element = window.parent.document.getElementsByTagName("footer");
-    element.appendChild(newElement);
-}
-function todayDate(){
-    var d = new Date();
-    var n = d.getFullYear() + "  ";
-    return window.parent.document.getElementsByTagName('p').innerHTML=n;
-  }
+// function insertParagraph(){
+//     var newElement = window.parent.document.createElement("p");
+//     // var node = document.createTextNode("new text");
+//     // newElement.appendChild(node);
+//     var element = window.parent.document.getElementsByTagName("footer");
+//     element.appendChild(newElement);
+// }
+// function todayDate(){
+//     var d = new Date();
+//     var n = d.getFullYear() + "  ";
+//     return window.parent.document.getElementsByTagName('p').innerHTML=n;
+//   }
   
-insertParagraph()
-// const elements = window.parent.document.getElementsByTagName('footer')
-// elements[0].innerHTML = "&copy; Omdena project"
+
+var newElement = window.parent.document.createElement('p');
+const elements = window.parent.document.getElementsByTagName('footer');
+elements.appendChild(newElement)
+elements[0].innerHTML = "&copy; Omdena project";
