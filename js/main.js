a4cc1f688b07c59536ec0bf72b4ccdba0ca42b3b
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-01 22:48:27 +1100

    copy right text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const elements = window.parent.document.getElementsByTagName('footer');
-elements[0].innerHTML = "&copy; Omdena project " + new Date().getFullYear();
+elements[0].innerHTML = "&copy;OmdenaAI benin chapter red blood cells " + new Date().getFullYear();
 
 const manageTerminal = window.parent.document.getElementsByClassName('styles_terminalButton__3xUnY');
 manageTerminal[0].innerHTML = "hello"
