3df5c5134cd68faf6e013e2e4e3b7fa73f4e36af
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-01 11:12:54 +1100

    edit style

--- js/main.js
+++ js/main.js
@@ -5,6 +5,5 @@ function todayDate(){
     return window.parent.document.getElementsByTagName("em").innerHTML = n;
   }
 const elements = window.parent.document.getElementsByTagName('footer')
-elements[0].innerHTML = "&copy;" 
-elements[1].innerHTML =" <em></em>"
-elements[2].innerHTML = "Omdena project"
+elements[0].innerHTML = "&copy; Omdena project" 
+elements[1].innerHTML = todayDate()
