77b33498ad67cc44cf2fe90f53d00e611157bb79
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-01 11:51:53 +1100

    edit style

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const elements = window.parent.document.getElementsByTagName('footer');
 elements[0].innerHTML = "&copy; Omdena project";
 elements[0].appendChild(window.parent.document.createElement('p'));
 const date_element = window.parent.document.getElementsByTagName('p');
-date_element[0].innerHTML = todayDate;
+date_element[0].innerHTML = new Date();
 // const element = window.parent.document.getElementsByTagName('footer');
 // element[0].appendChild(window.parent.document.createElement('p'));
 // window.parent.document.getElementsByTagName('p').innerHTML = todayDate()
