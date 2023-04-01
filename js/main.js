3adf7002ecaac60069105593383c1a2e2c433b2f
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-01 11:54:26 +1100

    copy right

--- js/main.js
+++ js/main.js
@@ -1,25 +1,2 @@
-
-// function insertParagraph(){
-//     var newElement = window.parent.document.createElement("p");
-//     // var node = document.createTextNode("new text");
-//     // newElement.appendChild(node);
-//     var element = window.parent.document.getElementsByTagName("footer");
-//     element.appendChild(newElement);
-// }
-function todayDate(){
-    var d = new Date();
-    var n = d.getFullYear() + "  ";
-    return n;
-  }
-  
-
-// var newElement = window.parent.document.createElement('p');
 const elements = window.parent.document.getElementsByTagName('footer');
-elements[0].innerHTML = "&copy; Omdena project" + new Date().getFullYear();
-// elements[0].appendChild(window.parent.document.createElement('p'));
-// const date_element = window.parent.document.getElementsByTagName('p');
-// date_element[0].innerHTML = new Date().getFullYear();
-// const element = window.parent.document.getElementsByTagName('footer');
-// element[0].appendChild(window.parent.document.createElement('p'));
-// window.parent.document.getElementsByTagName('p').innerHTML = todayDate()
-
+elements[0].innerHTML = "&copy; Omdena project " + new Date().getFullYear();
