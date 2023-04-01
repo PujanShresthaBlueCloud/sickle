76bddf187666f3380add76b07e56d891ac072fde
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-01 11:53:28 +1100

    added date

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@ function todayDate(){
 
 // var newElement = window.parent.document.createElement('p');
 const elements = window.parent.document.getElementsByTagName('footer');
-elements[0].innerHTML = "&copy; Omdena project";
-elements[0].appendChild(window.parent.document.createElement('p'));
-const date_element = window.parent.document.getElementsByTagName('p');
-date_element[0].innerHTML = new Date();
+elements[0].innerHTML = "&copy; Omdena project" + new Date().getFullYear();
+// elements[0].appendChild(window.parent.document.createElement('p'));
+// const date_element = window.parent.document.getElementsByTagName('p');
+// date_element[0].innerHTML = new Date().getFullYear();
 // const element = window.parent.document.getElementsByTagName('footer');
 // element[0].appendChild(window.parent.document.createElement('p'));
 // window.parent.document.getElementsByTagName('p').innerHTML = todayDate()
