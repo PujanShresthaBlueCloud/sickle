b9d3e68e752c29db0bd9f919b3ad220e92b3d59d
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-03 11:03:01 +1000

    email generation

--- pages/Generate_Report.py
+++ pages/Generate_Report.py
@@ -125,50 +125,48 @@ def email_form():
   if submit_button:
     if not re.match(email_regex, email):
       st.error("Please enter a valid email address")
-  else:
+    else:
       return email
 
 def send_email():
 # Define email button
   st.write("in send email")
-  email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-  email = st.text_input("Email")
-  if not re.match(email_regex, email) and email:
-    st.error("Please enter a valid email address")
-  elif st.button("Send Report by Email"):
-    st.write("in st button --")
-    # Define email message
-    message = MIMEMultipart()
-    message['To'] = email
-    message['Subject'] = 'Sickle cell detection report'
+  email = email_form()
+  if email is not None:
+    if st.button("Send Report by Email"):
+      st.write("in st button --")
+      # Define email message
+      message = MIMEMultipart()
+      message['To'] = email
+      message['Subject'] = 'Sickle cell detection report'
 
-    # Add some text to the message body
-    body = f"Hi {first_name}, please find your report in attachment."
-    message.attach(MIMEText(body, "plain"))
-    pdfkit.from_string(html, report)
-    # Attach a PDF file to the message
-    with open(report, "rb") as file:
-        attachment = MIMEApplication(file.read(), _subtype="pdf")
-        attachment.add_header(
-            "Content-Disposition",
-            "attachment",
-            filename=report
-        )
-        message.attach(attachment)
+      # Add some text to the message body
+      body = f"Hi {first_name}, please find your report in attachment."
+      message.attach(MIMEText(body, "plain"))
+      pdfkit.from_string(html, report)
+      # Attach a PDF file to the message
+      with open(report, "rb") as file:
+          attachment = MIMEApplication(file.read(), _subtype="pdf")
+          attachment.add_header(
+              "Content-Disposition",
+              "attachment",
+              filename=report
+          )
+          message.attach(attachment)
 
-    # Send the message
-    try:
-        smtp_username = "pujansth16@gmail.com"
-        smtp_password = "bmngcpaoruhencsd"
-        connection = s.SMTP('smtp.gmail.com', 587)
-        connection.starttls()
-        connection.login(smtp_username, smtp_password)
-        connection.sendmail(smtp_username, email, message.as_string())
-        connection.quit()
-        st.success("Email sent successfully!")
+      # Send the message
+      try:
+          smtp_username = "pujansth16@gmail.com"
+          smtp_password = "bmngcpaoruhencsd"
+          connection = s.SMTP('smtp.gmail.com', 587)
+          connection.starttls()
+          connection.login(smtp_username, smtp_password)
+          connection.sendmail(smtp_username, email, message.as_string())
+          connection.quit()
+          st.success("Email sent successfully!")
 
-    except Exception as e:
-        st.error(f"Error sending email: {e}")
+      except Exception as e:
+          st.error(f"Error sending email: {e}")
 
 
 if(html != ''):
