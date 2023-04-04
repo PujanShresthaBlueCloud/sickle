afb52127681be358045687360224be181fd0b749
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-04 12:21:01 +1000

    report generation

--- pages/Generate_Report.py
+++ pages/Generate_Report.py
@@ -129,45 +129,46 @@ def email_form():
       return email
 
 def send_email():
-    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    email = st.text_input("Email", placeholder="Enter email address")
-    if not re.match(email_regex, email):
-      st.error("Please enter a valid email address")
-    else:
-      # Define email button
-      if st.button("Send Report by Email"):
-        st.write("email :", email)
-        # Define email message
-        message = MIMEMultipart()
-        message['Subject'] = 'Sickle cell detection report'
+    if st.button("Send Report by Email")
+      email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+      email = st.text_input("Email", placeholder="Enter email address")
+      # if not re.match(email_regex, email):
+      #   st.error("Please enter a valid email address")
+      if email:
+        # Define email button
+        if st.button("Send"):
+          st.write("email :", email)
+          # Define email message
+          message = MIMEMultipart()
+          message['Subject'] = 'Sickle cell detection report'
 
-        # Add some text to the message body
-        body = f"Hi {first_name}, please find your report in attachment."
-        message.attach(MIMEText(body, "plain"))
-        pdfkit.from_string(html, report)
-        # Attach a PDF file to the message
-        with open(report, "rb") as file:
-            attachment = MIMEApplication(file.read(), _subtype="pdf")
-            attachment.add_header(
-                "Content-Disposition",
-                "attachment",
-                filename=report
-            )
-            message.attach(attachment)
+          # Add some text to the message body
+          body = f"Hi {first_name}, please find your report in attachment."
+          message.attach(MIMEText(body, "plain"))
+          pdfkit.from_string(html, report)
+          # Attach a PDF file to the message
+          with open(report, "rb") as file:
+              attachment = MIMEApplication(file.read(), _subtype="pdf")
+              attachment.add_header(
+                  "Content-Disposition",
+                  "attachment",
+                  filename=report
+              )
+              message.attach(attachment)
 
-        # Send the message
-        try:
-            smtp_username = "pujansth16@gmail.com"
-            smtp_password = "bmngcpaoruhencsd"
-            connection = s.SMTP('smtp.gmail.com', 587)
-            connection.starttls()
-            connection.login(smtp_username, smtp_password)
-            connection.sendmail(smtp_username, email, message.as_string())
-            connection.quit()
-            st.success("Email sent successfully!")
+          # Send the message
+          try:
+              smtp_username = "pujansth16@gmail.com"
+              smtp_password = "bmngcpaoruhencsd"
+              connection = s.SMTP('smtp.gmail.com', 587)
+              connection.starttls()
+              connection.login(smtp_username, smtp_password)
+              connection.sendmail(smtp_username, email, message.as_string())
+              connection.quit()
+              st.success("Email sent successfully!")
 
-        except Exception as e:
-            st.error(f"Error sending email: {e}")
+          except Exception as e:
+              st.error(f"Error sending email: {e}")
 
 
 if(html != ''):
