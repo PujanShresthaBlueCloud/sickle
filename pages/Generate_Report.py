b878e2f9786f09e11fade408c0247ca0e7156ed9
P <pujan_sth@yahoo.com>
2023-04-06 14:08:18 +1000

    detected result attached

--- pages/Generate_Report.py
+++ pages/Generate_Report.py
@@ -15,6 +15,14 @@ helper.local_css(settings.CSS)
 
 # Using custom js
 helper.local_js(settings.JS)
+
+# Clearing session id
+session_id = st.session_state._get_session_id()
+st.experimental_rerun()
+st.caching.clear_cache()
+# st.session_state.clear()
+st.stop()
+
 # Getting report
 detected_data_frame = st.session_state['detected_data_frame'] if st.session_state['detected_data_frame'] is not None else st.write("Generate object detection result first")
 detected_result = detected_data_frame.to_html(index=False)
