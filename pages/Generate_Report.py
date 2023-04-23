e9750867e333c3dd6975886df1f17a1dce9a74e4
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-23 14:28:59 +1000

    added generate file

--- pages/Generate_Report.py
+++ pages/Generate_Report.py
@@ -8,16 +8,14 @@ import re
 import helper
 import settings
 import email.utils
-from streamlit import _legacy_util
 
 # Get the current URL query string
-query_params = _legacy_util.get_query_params()
+query_params = st.experimental_get_query_params()
 
 # Get the value of the "my_var" parameter, or use a default value if it's not present
 detected_result = query_params.get("detected_result", ["default_value"])[0]
 
 st.write(detected_result)
-
 # st.set_page_config(keep_session_state=True)
 # Using custom css
 helper.local_css(settings.CSS)
