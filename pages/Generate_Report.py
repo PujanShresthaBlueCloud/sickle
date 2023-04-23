b179335e504b068f43038788a9204421b9f249ca
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-23 16:35:20 +1000

    conditioned

--- pages/Generate_Report.py
+++ pages/Generate_Report.py
@@ -18,7 +18,7 @@ helper.local_js(settings.JS)
 
 
 # Getting report
-detected_data_frame = st.session_state['detected_data_frame'] if st.session_state['detected_data_frame'] is not None else st.header("Generate object detection result first")
+detected_data_frame = st.session_state['detected_data_frame'] if st.session_state['detected_data_frame'] is not None else st.write("Generate object detection result first")
 
 if(detected_data_frame is not None):
   # Define Streamlit app title
