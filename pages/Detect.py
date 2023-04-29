821bf12c0ce3c4d9ce0cbd479095b87152a7a9c4
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-04-29 22:35:08 +1000

    detected disease

--- pages/Detect.py
+++ pages/Detect.py
@@ -117,18 +117,18 @@ if source_radio == settings.IMAGE:
                  ]
             detected_data_frame=pd.DataFrame(detected_cal, columns=['class','count','percent'], index=None)
             st.dataframe(detected_data_frame, use_container_width=True)
-            detected =[]
-            ['Crystal', 'Normal', 'Others', 'Sickle', 'Target']
-            detected['Crystal'] = crystal_percent
-            detected['Normal'] = normal_percent
-            detected['Others'] = others_percent
-            detected['Sickle'] = sickle_percent
-            detected['Target'] = target_percent
+            detected =[['Crystal', 'Normal', 'Others', 'Sickle', 'Target']]
+            
+            detected[0] = crystal_percent
+            detected[1] = normal_percent
+            detected[2] = others_percent
+            detected[3] = sickle_percent
+            detected[4] = target_percent
 
             detected_disease = helper.Status(detected)
             st.write(detected)
 
-            
+
             detected_result = detected_data_frame.to_html(index=False)
 
             st.session_state["detected_data_frame"] = detected_data_frame
